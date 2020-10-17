9a94c6e2563b948f72093a8e20c75186b4d80a01
kagunyan25 <mnm.0225kaguya@gmail.com>
2020-10-17 17:42:05 +0900

    3-2プロジェクトでコンポーネント開発！/Appコンポーネントを書き換えよう

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';//Reactオブジェクトの中のComponent
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return <div>
+      <h1>React</h1>
+      <p>This is sample component.</p>
+      <p>これはサンプルのコンポーネントです.</p>
+    </div>;
+  }
 }
 
-export default App;
+export default App;//class Appを使えるようにエクスポートしておく
