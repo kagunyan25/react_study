992d992fcae2bc88bdff06d4dda864b5867588ad
kagunyan25 <mnm.0225kaguya@gmail.com>
2020-10-19 01:40:47 +0900

    3-3ステートを使いこなそう/ステートを用意する

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -4,16 +4,29 @@ import './App.css';
 import Rect from './Rect.js';
 
 class App extends Component {
+  msgStyle = {
+    fontSize:"24pt",
+    color:"#900",
+    margin:"20px 0px",
+    padding:"5px",
+    borderBottom:"2px solid #900"
+  }
+
+  constructor(props){
+    super(props);
+    this.state={
+      msg:"Hello Component.",
+    };
+  }
+
   render(){
-    return<div>
-      <Rect x="50" y="50" w="150" h="150" c="#6ff9" r="50"/>
-      <Rect x="150" y="100" w="150" h="150" c="#f6f9"r="75"/>
-      <Rect x="100" y="150" w="150" h="150" c="#6669" r="25"/>
+    return <div>
+      <h1>React</h1>
+      <p style = {this.msgStyle}>{this.state.msg}</p>
+      <p style = {this.msgStyle}>{this.props.msg}</p>
     </div>;
   }
 }
-
-
 
 
 export default App;//class Appを使えるようにエクスポートしておく
