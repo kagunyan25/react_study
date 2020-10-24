14ba17b9db902192f3534021dd4690069d5880da
kagunyan25 <mnm.0225kaguya@gmail.com>
2020-10-25 03:09:12 +0900

    3-3ステートを使いこなそう/プロパティとステートの連携

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,58 +1,61 @@
 import React, {Component} from 'react';//Reactオブジェクトの中のComponent
 //import logo from './logo.svg';
 import './App.css';
-import Rect from './Rect.js';
+import Rect from './Rect';
 
 class App extends Component {
-  msgStyle1 = {
-    fontSize:"24pt",
+  data=[];//配列を保管するプロパティ　doActionでクリックした位置の情報を追加する.
+
+  msgStyle = {
+    fontSize:"20pt",
     color:"#900",
     margin:"20px 0px",
     padding:"5px",
-    borderBottom:"2px solid #900"
-  }
-  msgStyle2 = {
-    fontSize:"20pt",
-    color:"white",
-    backgroundColor:"#900",
-    margin:"20px 0px",
-    padding:"5px",
-    borderBottom:"2px solid #900"
   }
   
-  btnStyle = {
-    fontSize:"16pt",
-    padding:"10px"
+  area = {
+    width:"500px",
+    height:"500px",
+    padding:"1px solid blue",
   }
 
   constructor(props){
     super(props);
     this.state={//あくまでステートの初期化
-      counter:0,
-      msg:"count start!",
-      flg:true,
+      list:this.data
     };
     this.doAction=this.doAction.bind(this);
   }
 
     doAction(e){
-      this.setState((state)=>({
-        counter:state.counter+1,
-        msg:state.counter,
-        flg:!state.flg//クリックするたびにtrue->false,false->true
-      }));
+      let x=e.pageX;//ページの左上からクリックした地点までの距離
+      let y=e.pageY;//同様
+      this.data.push({x:x,y:y});//{x:横位置,y:縦位置}をthis.dataにpushで追加
+      this.setState({
+        list:this.data//listステートに設定
+      });
+    }
+
+    draw(d){
+      let s={
+        position:"absolute",
+        left:(d.x-25)+"px",
+        top:(d.y-25)+"px",
+        width:"50px",
+        height:"50px",
+        backgroundColor:"#66f3",
+      };
+      return <div style={s}></div>
     }
   
 
   render(){
     return <div>
       <h1>React</h1>
-      {this.state.flg ?
-      <p style={this.msgStyle1}>{this.state.msg}</p>//trueの時
-    :
-      <p style={this.msgStyle2}>{this.state.msg}です。</p>//falseの時
-    }
-      <button style={this.btnStyle} onClick={this.doAction}>Click</button>
+      <h2 style={this.msgStyle}>show rect.</h2>
+      <div style={this.area} onClick={this.doAction}>
+        {this.data.map((value)=>this.draw(value))}
+      </div>
     </div>;
   }
 }
