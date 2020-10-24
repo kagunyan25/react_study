93485b15925be0e2612e8b33a0dd8d40ad511742
kagunyan25 <mnm.0225kaguya@gmail.com>
2020-10-25 01:52:52 +0900

    3-3ステートを使いこなそう/ステートで表示を切り替える

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -4,16 +4,25 @@ import './App.css';
 import Rect from './Rect.js';
 
 class App extends Component {
-  msgStyle = {
+  msgStyle1 = {
     fontSize:"24pt",
     color:"#900",
     margin:"20px 0px",
     padding:"5px",
     borderBottom:"2px solid #900"
   }
-  btnStyle = {
+  msgStyle2 = {
     fontSize:"20pt",
-    padding:"0px 10px"
+    color:"white",
+    backgroundColor:"#900",
+    margin:"20px 0px",
+    padding:"5px",
+    borderBottom:"2px solid #900"
+  }
+  
+  btnStyle = {
+    fontSize:"16pt",
+    padding:"10px"
   }
 
   constructor(props){
@@ -21,6 +30,7 @@ class App extends Component {
     this.state={//あくまでステートの初期化
       counter:0,
       msg:"count start!",
+      flg:true,
     };
     this.doAction=this.doAction.bind(this);
   }
@@ -28,7 +38,8 @@ class App extends Component {
     doAction(e){
       this.setState((state)=>({
         counter:state.counter+1,
-        msg:'count:'+state.counter
+        msg:state.counter,
+        flg:!state.flg//クリックするたびにtrue->false,false->true
       }));
     }
   
@@ -36,7 +47,11 @@ class App extends Component {
   render(){
     return <div>
       <h1>React</h1>
-      <p style = {this.msgStyle}>{this.state.msg}</p>
+      {this.state.flg ?
+      <p style={this.msgStyle1}>{this.state.msg}</p>//trueの時
+    :
+      <p style={this.msgStyle2}>{this.state.msg}です。</p>//falseの時
+    }
       <button style={this.btnStyle} onClick={this.doAction}>Click</button>
     </div>;
   }
