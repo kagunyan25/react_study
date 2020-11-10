88f5156e06b7347b7c8ff9b047e1e5a4e093a486
kagunyan25 <mnm.0225kaguya@gmail.com>
2020-11-10 16:50:31 +0900

    4-1Reduxを使ってみよう/ストアを作成してみよう

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
 
+//ストアに保管するステートの値
+let state_value={
+  conter:0,
+  message:"COUNTER"
+}
+
+//レデューサー
+function counter(state=state_value, action){//action…呼び出しの内容をまとめたオブジェクト
+  switch(action.type){
+    case "INCREMENT":
+      return{
+        counter:state.counter+1,
+        message:"INCREMENT"
+      };
+    case "DECREMENT":
+      return{
+        counter:state.counter-1,
+        message:"DECREMENT"
+      };
+    default:
+      return state;
+  }
+}
+
+//レデューサーをもとにストアを作成
+let store = createStore(counter);
+
+//表示をレンダリング
 ReactDOM.render(
-  <App msg="Hello App."/>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
